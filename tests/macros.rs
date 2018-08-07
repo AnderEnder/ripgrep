4c4fcb7ab7fe2161229a1c26f0d36f07f9f272df
Andrew Gallant <jamslam@gmail.com>
2018-08-06 20:11:58 -0400

    tests: re-tool integration tests
    This basically rewrites every integration test. We reduce the amount of
    magic involved here in terms of which arguments are being passed to
    ripgrep processes. To make up for the boiler plate saved by the magic,
    we make the Dir (formerly WorkDir) type a bit nicer to use, along with a
    new TestCommand that wraps a std::process::Command. In exchange, we get
    tests that are easier to read and write.

---
+++ tests/macros.rs
@@ -0,0 +1,56 @@
+#[macro_export]
+macro_rules! rgtest {
+    ($name:ident, $fun:expr) => {
+        #[test]
+        fn $name() {
+            let (dir, cmd) = ::util::setup(stringify!($name));
+            $fun(dir, cmd);
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! eqnice {
+    ($expected:expr, $got:expr) => {
+        let expected = &*$expected;
+        let got = &*$got;
+        if expected != got {
+            panic!("
+printed outputs differ!
+
+expected:
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+got:
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+", expected, got);
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! eqnice_repr {
+    ($expected:expr, $got:expr) => {
+        let expected = &*$expected;
+        let got = &*$got;
+        if expected != got {
+            panic!("
+printed outputs differ!
+
+expected:
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{:?}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+got:
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+{:?}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+", expected, got);
+        }
+    }
+}
